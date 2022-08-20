5fa22fb119aedf7f9c6a57c9ee37b2fe9e1de0d6
tsumitakaoru <k.tsumita01@gmail.com>
2022-08-20 14:19:04 -0400

    Get random 1 to 6

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 const image1 = document.querySelector(".img1");
 const image2 = document.querySelector(".img2");
+
+const randomNumber1  = Math.floor(Math.random() * 6 + 1);
+const randomNumber2  = Math.floor(Math.random() * 6 + 1);
