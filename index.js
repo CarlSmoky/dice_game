f21907b660c931a12e308bfb48242ae42282f544
tsumitakaoru <k.tsumita01@gmail.com>
2022-08-20 14:20:05 -0400

    Set Attribute for imgs

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const image2 = document.querySelector(".img2");
 
 const randomNumber1  = Math.floor(Math.random() * 6 + 1);
 const randomNumber2  = Math.floor(Math.random() * 6 + 1);
+
+image1.setAttribute("src", `images/dice${randomNumber1}.png`);
+image2.setAttribute("src", `images/dice${randomNumber2}.png`);
