81aff9b8acc23e79808bbb4bb99d5116aeb7ae1d
tsumitakaoru <k.tsumita01@gmail.com>
2022-08-20 14:27:31 -0400

    Fix issue

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const image1 = document.querySelector(".img1");
 const image2 = document.querySelector(".img2");
 
-const randomNumber1  = Math.floor(Math.random() * 6 + 1);
-const randomNumber2  = Math.floor(Math.random() * 6 + 1);
+const randomNumber1  = Math.floor(Math.random() * 6)+ 1;
+const randomNumber2  = Math.floor(Math.random() * 6) + 1;
 
 image1.setAttribute("src", `images/dice${randomNumber1}.png`);
 image2.setAttribute("src", `images/dice${randomNumber2}.png`);
