646859faa1f981d0752565011ea016c91e4c2e6f
tsumitakaoru <k.tsumita01@gmail.com>
2022-08-20 14:20:36 -0400

    Add judge function

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ const randomNumber2  = Math.floor(Math.random() * 6 + 1);
 
 image1.setAttribute("src", `images/dice${randomNumber1}.png`);
 image2.setAttribute("src", `images/dice${randomNumber2}.png`);
+
+const judge = () => {
+  let displayText = "";
+  if (randomNumber1 > randomNumber2) {
+    displayText = "Player 1 Wins!";
+  } else if (randomNumber1 < randomNumber2) {
+    displayText = "Player 2 Wins!";
+  } else if (randomNumber1 === randomNumber2) {
+    displayText = "Draw!";
+  }
+  return displayText;
+};
